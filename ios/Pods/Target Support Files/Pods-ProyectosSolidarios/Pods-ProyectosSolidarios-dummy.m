d457eab5fe5b6004f690ef5246646917eec6e114
Nicolás Andrés Gallinal <ngallinal@hexacta.com>
2020-07-03 12:51:33 -0300

    implementation of firebase analytics in iOS using cocoapods

---
+++ ios/Pods/Target Support Files/Pods-ProyectosSolidarios/Pods-ProyectosSolidarios-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ProyectosSolidarios : NSObject
+@end
+@implementation PodsDummy_Pods_ProyectosSolidarios
+@end
---
+++ ios/Pods/Target Support Files/Pods-ProyectosSolidariosTests/Pods-ProyectosSolidariosTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ProyectosSolidariosTests : NSObject
+@end
+@implementation PodsDummy_Pods_ProyectosSolidariosTests
+@end
