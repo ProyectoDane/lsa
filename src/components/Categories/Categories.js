185ee741a5f00d18267eae96c55b4d9847622df2
julietaflux <julietaflux@gmail.com>
2020-07-26 14:02:18 -0300

    34007 refactor: categories creation and rendering refactors

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,103 +1,34 @@
 import React, { PureComponent } from 'react';
-import {
-  Text,
-  View,
-  Image,
-  FlatList,
-  Dimensions,
-  TouchableOpacity,
-  ImageBackground,
-} from 'react-native';
+import { View, FlatList, Dimensions, ImageBackground } from 'react-native';
 import _ from 'lodash';
 
 import CATEGORIES_INDEX from './../../categoriesIndex';
 import { PAGES } from './../../constants/';
-import { getCardWidth, getCardsPerRow, getCardPadding } from './../../util/layoutUtil';
+import { getCardsPerRow, getCardPadding } from './../../util/layoutUtil';
+import { CategoryCard } from './CategoryCard';
 import styles from './styles';
 // Analytics
 import firebase from 'react-native-firebase';
 
+const fondoAmarillo = require('./../../res/background/fondo-amarillo.jpg');
+
 const Analytics = firebase.analytics();
 
 export class Categories extends PureComponent {
-  componentDidMount (){
-    this._navigateToCategory= _.debounce(this._navigateToCategory.bind(this), 500);
+  constructor(props) {
+    super(props);
+    this.state = {
+      categoriesChunks: this._getCategoriesChunks(),
+    };
   }
-  _navigateToCategory = category => {
-    const { navigation } = this.props;
-    Analytics.logEvent("category_view", {category:category.name_es});
-    navigation.navigate(PAGES.PAGE_CATEGORY, { category });
-  };
 
-  _renderCategory = (category, imagePaddingHorizontal, imagePaddingVertical) => (
-    <TouchableOpacity
-      onPress={() => this._navigateToCategory(category)}
-      key={category.name_es}
-      style={[
-        styles.categoryContainer,
-        {
-          width: getCardWidth(),
-          paddingVertical: getCardPadding(),
-          paddingHorizontal: getCardPadding(),
-        },
-      ]}
-    >
-      <View
-        style={[
-          styles.imageContainer,
-          {
-            width: getCardWidth() - 2 * getCardPadding(),
-            height: getCardWidth() - 4 * getCardPadding(),
-          },
-        ]}
-      >
-        <Image
-          style={[
-            styles.categoryIcon,
-            {
-              width: getCardWidth() - 2 * (getCardPadding() + imagePaddingHorizontal),
-              height: getCardWidth() - 2 * (getCardPadding() + imagePaddingVertical),
-            },
-          ]}
-          source={category.icon}
-        />
-      </View>
-      <View
-        style={[
-          styles.categoryNameContainer,
-          {
-            width: getCardWidth() - 2 * getCardPadding(),
-            paddingHorizontal: imagePaddingHorizontal,
-          },
-        ]}
-      >
-        <Text style={styles.categoryName}>{category.name_es}</Text>
-      </View>
-    </TouchableOpacity>
-  );
-
-  _keyExtractor = (item, index) => `CATEGORY${this.props.albumId}ROW${index}`;
-
-  _renderItem = ({ item, index }) => {
-    const isLastRow = index === this.rowsCount - 1;
-    return (
-      <View
-        style={
-          isLastRow
-            ? [styles.lastRowContainer, { marginBottom: getCardPadding() * 2 }]
-            : styles.rowContainer
-        }
-      >
-        {item.map(itemTwo => {
-          return this._renderCategory(
-            itemTwo,
-            this.imagePaddingHorizontal,
-            this.imagePaddingVertical
-          );
-        })}
-      </View>
-    );
-  };
+  componentDidMount() {
+    Dimensions.addEventListener('change', () => {
+      this.setState({
+        categoriesChunks: this._getCategoriesChunks(),
+      });
+    });
+  }
 
   componentWillReceiveProps(nextProps) {
     const { dispatchCategoriesRestarted } = this.props;
@@ -107,24 +38,54 @@ export class Categories extends PureComponent {
     }
   }
 
-  _onLayout = () => {
-    this.forceUpdate();
+  _getCategoriesChunks() {
+    return _.chunk(CATEGORIES_INDEX.categories, getCardsPerRow());
+  }
+
+  _navigateToCategory = _.debounce(category => {
+    const { navigation } = this.props;
+    Analytics.logEvent('category_view', { category: category.name_es });
+    navigation.navigate(PAGES.PAGE_CATEGORY, { category });
+  }, 500);
+
+  _keyExtractor = (item, index) => `CATEGORY${this.props.albumId}ROW${index}`;
+
+  _renderRow = ({ item, index }) => {
+    const isLastRow = index === this.state.categoriesChunks.length - 1;
+    return (
+      <View
+        style={
+          isLastRow
+            ? [styles.lastRowContainer, { marginBottom: getCardPadding() * 2 }]
+            : styles.rowContainer
+        }
+      >
+        {item.map(category => (
+          <CategoryCard
+            category={category}
+            navigateToCategory={this._navigateToCategory}
+            key={category.name_es}
+          />
+        ))}
+      </View>
+    );
   };
 
   render() {
-    this.imagePaddingHorizontal = getCardPadding() * 2;
-    this.imagePaddingVertical = getCardPadding() * 2;
-    const categoriesChunks = _.chunk(CATEGORIES_INDEX.categories, getCardsPerRow());
-    this.rowsCount = categoriesChunks.length;
     return (
-      <View style={styles.full} onLayout={this._onLayout}>
+      <View
+        style={styles.full}
+        onLayout={() => {
+          this.forceUpdate();
+        }}
+      >
         <ImageBackground
           style={styles.full}
           imageStyle={[
             styles.backgroundImageStyle,
             { width: Dimensions.get('window').width, height: Dimensions.get('window').height },
           ]}
-          source={require('./../../res/background/fondo-amarillo.jpg')}
+          source={fondoAmarillo}
         >
           <FlatList
             ref={list => {
@@ -134,10 +95,10 @@ export class Categories extends PureComponent {
               styles.categoriesViewContainer,
               { paddingVertical: getCardPadding(), paddingHorizontal: getCardPadding() },
             ]}
-            removeClippedSubviews={true}
-            data={categoriesChunks}
+            removeClippedSubviews
+            data={this.state.categoriesChunks}
             keyExtractor={this._keyExtractor}
-            renderItem={this._renderItem}
+            renderItem={this._renderRow}
           />
         </ImageBackground>
       </View>
