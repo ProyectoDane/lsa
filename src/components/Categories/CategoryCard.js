ccfe2933c4edb6f391a2238030db1a1fcf1be2ff
julietaflux <julietaflux@gmail.com>
2020-08-09 16:01:41 -0300

    lint fixes

--- src/components/Categories/CategoryCard.js
+++ src/components/Categories/CategoryCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Text, Image, View, TouchableOpacity } from 'react-native';
-import { getCardWidth, getCardPadding } from './../../util/layoutUtil';
+import {Text, Image, View, TouchableOpacity} from 'react-native';
+import {getCardWidth, getCardPadding} from './../../util/layoutUtil';
 import styles from './styles';
 
 const imagePaddingHorizontal = getCardPadding() * 2;
 const imagePaddingVertical = getCardPadding() * 2;
 
-export const CategoryCard = ({ category, navigateToCategory }) => (
+export const CategoryCard = ({category, navigateToCategory}) => (
   <TouchableOpacity
     onPress={() => navigateToCategory(category)}
     style={[
@@ -16,8 +16,7 @@ export const CategoryCard = ({ category, navigateToCategory }) => (
         paddingVertical: getCardPadding(),
         paddingHorizontal: getCardPadding(),
       },
-    ]}
-  >
+    ]}>
     <View
       style={[
         styles.imageContainer,
@@ -25,14 +24,15 @@ export const CategoryCard = ({ category, navigateToCategory }) => (
           width: getCardWidth() - 2 * getCardPadding(),
           height: getCardWidth() - 4 * getCardPadding(),
         },
-      ]}
-    >
+      ]}>
       <Image
         style={[
           styles.categoryIcon,
           {
-            width: getCardWidth() - 2 * (getCardPadding() + imagePaddingHorizontal),
-            height: getCardWidth() - 2 * (getCardPadding() + imagePaddingVertical),
+            width:
+              getCardWidth() - 2 * (getCardPadding() + imagePaddingHorizontal),
+            height:
+              getCardWidth() - 2 * (getCardPadding() + imagePaddingVertical),
           },
         ]}
         source={category.icon}
@@ -45,8 +45,7 @@ export const CategoryCard = ({ category, navigateToCategory }) => (
           width: getCardWidth() - 2 * getCardPadding(),
           paddingHorizontal: imagePaddingHorizontal,
         },
-      ]}
-    >
+      ]}>
       <Text style={styles.categoryName}>{category.name_es}</Text>
     </View>
   </TouchableOpacity>
